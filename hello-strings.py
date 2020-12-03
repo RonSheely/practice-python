958ea7551ee90dab46a9274e949b0f6514f80933
RonSheely <ron.sheely@ironwoodlabs.com>
2020-12-03 13:09:59 -0800

    commented obligatory hello programs.

---
+++ hello-strings.py
@@ -0,0 +1,19 @@
+# Define some arbitrary strings and print them.
+message1 = "Who are you?"
+message2 = 'I am your "helpful" guide.'
+message3 = "I'm going to help you find what you're look for."
+
+message4 = """If you would like me to help you tomorrow,
+              I can send you my contact information.
+              If you would like reschedule, please let me know."""
+# Print all the strings.
+# Note that long triple-quoted string format is awkward.
+# The printed output does not reflect format in the source code.
+# Todo - Learn to format strings so they are more easily managed.
+
+print(message1)
+print(message2)
+print(message3)
+print(message4)
+
+# Todo - How might strings be formated suitable for an interactive fiction game?
---
+++ hello-world.py
@@ -0,0 +1,8 @@
+# Print "Hello World!", the first step in a Python learning adventure.
+print("Hello World!")
+
+# Determine if this is the main program or module.
+if __name__ == __name__ == "__main__":
+    print("This is the main program.")
+else:
+    print("This is the " + __name__ + " module.")
